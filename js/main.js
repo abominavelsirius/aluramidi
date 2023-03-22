b80e3ae3889fc2d840d45628cbf0e7a69506d872
Abominavel_Sirius <abominavelsirius@gmail.com>
2023-03-22 14:34:06 -0300

    refazendo o site

--- js/main.js
+++ js/main.js
@@ -1,8 +1,1 @@
-function tocaSomPom () {
-    document.querySelector('.tecla_pom') .play();
-}
-
-
-const listaDeTeclas = document.querySelector('.tecla');
-
-listaDeTeclas[0] .onclick = tocaSomPom;
+document.querySelector('.tecla_pom');
