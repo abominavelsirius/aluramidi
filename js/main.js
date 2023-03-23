a0b3a6fed2a25bddab512270edb80a45a43f3de1
Abominavel_Sirius <abominavelsirius@gmail.com>
2023-03-23 14:48:05 -0300

    criando a function

--- js/main.js
+++ js/main.js
@@ -1,1 +1,12 @@
-document.querySelector('.tecla_pom');
+function tocaSomPom () {
+    
+    
+    document.querySelector('#som_tecla_pom').play(); // vai dá play no audio
+    
+}
+
+document.querySelector('.tecla_pom').onclick = tocaSomPom; 
+// o ponto depois de tecla_pom) vai entrar no documento que tá sendo chamado e terá seus vcalores também
+// o onclick será a mesma coisa do HTML, só que como uma boa prática dentro do jS
+// É importante guardar a função e não chamar
+// e pra fazer isso, é necessário tirar o () da function
