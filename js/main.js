d2101513a834c6a9120090d3a59eb3817e9a2c2e
Abominavel_Sirius <abominavelsirius@gmail.com>
2023-03-23 15:47:59 -0300

    textos dinamicos

--- js/main.js
+++ js/main.js
@@ -11,13 +11,17 @@ let contador = 0;
 // enquanto
 while (contador < listaDeTeclas.length) {
 
-    listaDeTeclas[contador].onclick = fuction () {
-        tocaSom ();
+    const tecla = listaDeTeclas[contador];
+    const instrumento = tecla.classList[1];
+    //template string
+    const idAudio = `#som_${instrumento}`;
+
+    tecla.onclick = function () {
+        tocaSom(idAudio);
     }
 
     contador = contador + 1;
 
-    console.log(contador);
 
 }
 
