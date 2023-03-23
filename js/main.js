018c8699a9a6d0b38ef009841c0473b4d1ed903e
Abominavel_Sirius <abominavelsirius@gmail.com>
2023-03-23 15:09:17 -0300

    listas

--- js/main.js
+++ js/main.js
@@ -1,11 +1,18 @@
 function tocaSomPom () {
-    
-    
+
     document.querySelector('#som_tecla_pom').play(); // vai dá play no audio
-    
+
 }
 
-document.querySelector('.tecla_pom').onclick = tocaSomPom; 
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+listaDeTeclas[0].onclick = tocaSomPom;
+
+
+
+
+
+// document.querySelector('.tecla_pom').onclick = tocaSomPom; 
 // o ponto depois de tecla_pom) vai entrar no documento que tá sendo chamado e terá seus vcalores também
 // o onclick será a mesma coisa do HTML, só que como uma boa prática dentro do jS
 // É importante guardar a função e não chamar
